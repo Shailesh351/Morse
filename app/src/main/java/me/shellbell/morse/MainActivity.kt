548e938334ef309e0578de16dae5d93c9ad3fdda
Shailesh Baldaniya <shaileshbaldaniya351@gmail.com>
2019-06-22 20:18:49 +0530

    feat: Set light theme

--- app/src/main/java/me/shellbell/morse/MainActivity.kt
+++ app/src/main/java/me/shellbell/morse/MainActivity.kt
@@ -1,12 +1,20 @@
 package me.shellbell.morse
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.ActionBar
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setUpActionBar()
+    }
+
+    private fun setUpActionBar() {
+        supportActionBar?.displayOptions = ActionBar.DISPLAY_SHOW_CUSTOM
+        supportActionBar?.setCustomView(R.layout.appbar)
     }
 }
